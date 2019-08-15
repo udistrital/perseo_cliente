9dfb6b03fb44fbb1d719c0dbfab8657fbf303e5d
ManuelMurillo <jmmurilloy@correo.udistrital.edu.co>
2019-08-15 16:13:03 -0500

    formulario con elementos

--- src/app/pages/acta-recibido/registro-acta-recibido/registro-acta-recibido.component.ts
+++ src/app/pages/acta-recibido/registro-acta-recibido/registro-acta-recibido.component.ts
@@ -3,6 +3,7 @@ import { LocalDataSource } from 'ngx-smart-table';
 import { Subscription } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+import { DatosLocales } from './datos_locales';
 
 
 @Component({
@@ -17,18 +18,7 @@ export class RegistroActaRecibidoComponent implements OnInit {
   // Datos de Tabla
   source: LocalDataSource;
 
-  ELEMENT_DATA: any[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
+  ELEMENT_DATA: any[]; // = DatosLocales;
 
   // Mensaje de Error
   errMess: any;
@@ -59,129 +49,6 @@ export class RegistroActaRecibidoComponent implements OnInit {
   @ViewChild('fform3') thirdFormDirective;
   Datos_Adicionales: any;
 
-  settings = {
-    actions: {
-      columnTitle: 'Acciones',
-      position: 'right',
-    },
-    add: {
-      addButtonContent: '<i class="nb-plus"></i>',
-      createButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-    },
-
-    edit: {
-      editButtonContent: '<i class="nb-edit"></i>',
-      saveButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-    },
-    delete: {
-      deleteButtonContent: '<i class="nb-trash"></i>',
-    },
-    columns: {
-      tipo_bien_id: {
-        title: 'Tipo de Bien',
-        valuePrepareFunction: (value: any) => {
-          return value;
-        },
-        filter: {
-          type: 'list',
-          config: {
-            selectText: 'Select...',
-            list: [
-              { value: '1', title: 'Devolutivo' },
-              { value: '2', title: 'Consumo' },
-              { value: '3', title: 'Consumo Controlado' },
-            ],
-          },
-        },
-        editor: {
-          type: 'list',
-          config: {
-            selectText: 'Select...',
-            list: [
-              { value: '1', title: 'Devolutivo' },
-              { value: '2', title: 'Consumo' },
-              { value: '3', title: 'Consumo Controlado' },
-            ],
-          },
-        },
-      },
-      subgrupo_id: {
-        title: 'Subgrupo',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      descripcion: {
-        title: 'Descripcion',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      cantidad: {
-        title: 'Cantidad',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      marca: {
-        title: 'Marca',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      serie: {
-        title: 'Serie',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      unidad_medida: {
-        title: 'Unidad de Medida',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      valor_unitario: {
-        title: 'Valor Unitario',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      valor_cantidad: {
-        title: 'Valor por Cantidad',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      descuento: {
-        title: 'Descuento',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      porcentaje_iva: {
-        title: '% IVA',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      valor_iva: {
-        title: 'Valor IVA',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-      valor_total: {
-        title: 'Valor Total',
-        valuePrepareFunction: (value) => {
-          return value;
-        },
-      },
-    },
-  };
-
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -201,20 +68,34 @@ export class RegistroActaRecibidoComponent implements OnInit {
       Dependencia: ['', Validators.required],
       Ubicacion: ['', Validators.required],
     });
-
     this.secondForm = this.fb.array([]);
+    const Elementos = this.fb.group({
+      TipoBienId: ['', Validators.required],
+      SubgrupoCatalogoId: ['', Validators.required],
+      Nombre: ['', Validators.required],
+      RevisorId: ['', Validators.required],
+      Cantidad: ['', Validators.required],
+      Marca: ['', Validators.required],
+      Serie: ['', Validators.required],
+      UnidadMedida: ['', Validators.required],
+      ValorUnitario: ['', Validators.required],
+      Subtotal: ['', Validators.required],
+      Descuento: ['', Validators.required],
+      PorcentajeIvaId: ['', Validators.required],
+      ValorIva: ['', Validators.required],
+      ValorTotal: ['', Validators.required],
+    });
     const Carga = this.fb.group({
       Proveedor: ['', Validators.required],
       Consecutivo: ['', Validators.required],
       Fecha_Factura: ['', Validators.required],
       Soporte: ['', Validators.required],
+      Elementos: this.fb.array([Elementos]),
     });
     this.secondForm.push(Carga);
-
     this.thirdForm = this.fb.group({
       Datos_Adicionales: ['', Validators.required],
     });
-
     this.carga_agregada = true;
   }
   ver() {
@@ -239,13 +120,31 @@ export class RegistroActaRecibidoComponent implements OnInit {
     this.tabs.push('Soporte ' + (this.tabs.length + 1));
     this.selected.setValue(this.tabs.length - 1);
     this.carga_agregada = true;
+    const Elementos = this.fb.group({
+      TipoBienId: ['', Validators.required],
+      SubgrupoCatalogoId: ['', Validators.required],
+      Nombre: ['', Validators.required],
+      RevisorId: ['', Validators.required],
+      Cantidad: ['', Validators.required],
+      Marca: ['', Validators.required],
+      Serie: ['', Validators.required],
+      UnidadMedida: ['', Validators.required],
+      ValorUnitario: ['', Validators.required],
+      Subtotal: ['', Validators.required],
+      Descuento: ['', Validators.required],
+      PorcentajeIvaId: ['', Validators.required],
+      ValorIva: ['', Validators.required],
+      ValorTotal: ['', Validators.required],
+    });
     const Carga = this.fb.group({
       Proveedor: ['', Validators.required],
       Consecutivo: ['', Validators.required],
       Fecha_Factura: ['', Validators.required],
       Soporte: ['', Validators.required],
+      Elementos: this.fb.array([Elementos]),
     });
     this.secondForm.push(Carga);
+    console.log(this.secondForm.get('Elementos.Nombre'));
   }
   removeTab(i: number) {
 
@@ -284,8 +183,24 @@ export class RegistroActaRecibidoComponent implements OnInit {
       }
     }
   }
-// Acciones para elementos
+  // Acciones para elementos
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns = [
+    'TipoBienId',
+    'SubgrupoCatalogoId',
+    'Nombre',
+    'RevisorId',
+    'Cantidad',
+    'Marca',
+    'Serie',
+    'UnidadMedida',
+    'ValorUnitario',
+    'Subtotal',
+    'Descuento',
+    'PorcentajeIvaId',
+    'ValorIva',
+    'ValorTotal',
+    'Acciones'
+  ];
   dataSource = this.ELEMENT_DATA;
 }
