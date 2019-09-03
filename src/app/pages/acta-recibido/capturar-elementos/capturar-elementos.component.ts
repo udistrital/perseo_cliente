cd88732ee86b0fadb5ec0b3e43e4fd7e5beadfff
ManuelMurillo <jmmurilloy@correo.udistrital.edu.co>
2019-09-03 17:36:58 -0500

    carga y lectura excel #67 #68

--- src/app/pages/acta-recibido/capturar-elementos/capturar-elementos.component.ts
+++ src/app/pages/acta-recibido/capturar-elementos/capturar-elementos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'ngx-capturar-elementos',
@@ -6,11 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./capturar-elementos.component.scss']
 })
 export class CapturarElementosComponent implements OnInit {
+  fileString: string | ArrayBuffer;
+  arrayBuffer: Iterable<number>;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  
+  cargandoImagen(event): void {
+    console.log(event);
+    this.readThis(event.target);
+  }
+
+  readThis(inputValue: any): void {
+
+    var file: File = inputValue.files[0];
+    var myReader: FileReader = new FileReader();
+    var fileType = inputValue.parentElement.id;
+
+    myReader.onloadend = (e) => {
+      
+      console.log(myReader.result);
+      this.fileString = myReader.result;
+
+            var workbook = XLSX.read(this.fileString, {type:"array"});
+            
+            console.log(workbook.Sheets);
+   };
+
+    myReader.readAsArrayBuffer(file);
+  }
+
 }
