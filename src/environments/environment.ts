9f9ed656a67f422bc0673f6b70a86aedc3333c72
Mafe Avendaño <mafe.ud@gmail.com>
2019-09-24 09:46:12 -0500

    token local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -23,7 +23,7 @@ export const environment = {
   UNIDADES_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_amazon_api/v1/',
   TOKEN: {
     AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
-    CLIENTE_ID: '3Idp5LUlnZY7cOV10NaLuyRfzooa',
+    CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
     RESPONSE_TYPE: 'id_token token',
     SCOPE: 'openid email role documento',
     REDIRECT_URL: 'http://localhost:4200/',
