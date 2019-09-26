5945b5a457f4d26ef6ddf3e98810a0903775fd67
Mafe Avendaño <mfavendannom@correo.udistrital.edu.co>
2019-09-26 13:39:17 -0500

    Update auth.Interceptor.ts

--- src/app/@core/_Interceptor/auth.Interceptor.ts
+++ src/app/@core/_Interceptor/auth.Interceptor.ts
@@ -26,7 +26,6 @@ export class AuthInterceptor implements HttpInterceptor {
       const authReq = req.clone({
         headers: new HttpHeaders({
           'Accept': 'application/json',
-          'Content-Type': 'application/json',
           'Authorization': `Bearer ${acces_token}`,
         }),
       });
