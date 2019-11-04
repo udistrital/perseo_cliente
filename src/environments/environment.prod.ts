9c97717170a4420902feb1002908ab7bb56c0947
Mafe Avendaño <mfavendannom@correo.udistrital.edu.co>
2019-11-04 18:38:21 -0500

    Ajuste de endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -17,6 +17,16 @@ export const environment = {
   ENTRADAS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/entradas_crud/v1/',
   ARKA_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/arka_mid/v1/',
   UNIDADES_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_amazon_api/v1/',
+  CATALOGO_BIENES_SERVICE: 'http://10.20.2.134:8087/v1/',
+  CATALOGO_ELEMENTOS_SERVICE: 'http://10.20.2.134:8087/v1/',
+  SPAGOBI: {
+    PROTOCOL: 'https',
+    HOST: 'intelligentia.udistrital.edu.co',
+    PORT: '8443',
+    CONTEXTPATH: 'SpagoBI',
+    USER: 'sergio_orjuela',
+    PASSWORD: 'sergio_orjuela',
+  },
   TOKEN: {
     AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
     CLIENTE_ID: '3Idp5LUlnZY7cOV10NaLuyRfzooa',
