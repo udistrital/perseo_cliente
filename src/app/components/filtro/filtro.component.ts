d8d9170b90ea812791a01c188fa4b7836ed0d883
Brayanb24 <babermudezb@correo.udistrital.edu.co>
2019-12-02 09:37:22 -0500

    linting terminando logica de peticiones #12

--- src/app/components/filtro/filtro.component.ts
+++ src/app/components/filtro/filtro.component.ts
@@ -28,7 +28,8 @@ export class FiltroComponent implements OnInit {
     console.info(this.numero_contrato);
     console.info(this.vigencia);
 
-    if (((isNaN(this.numero_contrato) === true) || (this.numero_contrato === 0) || (this.numero_contrato === null) || (this.numero_contrato === undefined)) && ((isNaN(this.identificacion_proveedor) === true) || (this.identificacion_proveedor === 0) || (this.identificacion_proveedor === null) || (this.identificacion_proveedor === undefined))) {
+    if (((isNaN(this.numero_contrato) === true) || (this.numero_contrato === 0) || (this.numero_contrato === null) || (this.numero_contrato === undefined)) 
+    && ((isNaN(this.identificacion_proveedor) === true) || (this.identificacion_proveedor === 0) || (this.identificacion_proveedor === null) || (this.identificacion_proveedor === undefined))) {
      this.openWindow('Debe ingresar almenos una Identificación de proveedor o un número de contrato');
     } else {
       this.RealizarPeticion();
@@ -36,16 +37,20 @@ export class FiltroComponent implements OnInit {
   }
 
   RealizarPeticion() {
-    if((this.identificacion_proveedor != undefined)&&(this.identificacion_proveedor != null) && (this.numero_contrato === undefined || this.numero_contrato === null) && (this.vigencia === undefined)){
+    if((this.identificacion_proveedor != undefined)&&(this.identificacion_proveedor != null) 
+    && (this.numero_contrato === undefined || this.numero_contrato === null) && (this.vigencia === undefined)){
       console.info('petición por proveedor')
     } else {
-      if((this.identificacion_proveedor != undefined)&&(this.identificacion_proveedor != null) && (this.numero_contrato === undefined || this.numero_contrato === null) && (this.vigencia != undefined)){
+      if((this.identificacion_proveedor != undefined)&&(this.identificacion_proveedor != null) 
+      && (this.numero_contrato === undefined || this.numero_contrato === null) && (this.vigencia != undefined)){
         console.info('peticion por proveedor y vigencia')
       } else {
-        if ((this.identificacion_proveedor === undefined || this.identificacion_proveedor === null) && (this.numero_contrato != undefined && this.numero_contrato != null) && (this.vigencia === undefined)){
+        if ((this.identificacion_proveedor === undefined || this.identificacion_proveedor === null) 
+        && (this.numero_contrato != undefined && this.numero_contrato != null) && (this.vigencia === undefined)){
           console.info('petición por número de contrato')
         } else {
-          if ((this.identificacion_proveedor === undefined || this.identificacion_proveedor === null) && (this.numero_contrato != undefined && this.numero_contrato != null) && (this.vigencia != undefined)){
+          if ((this.identificacion_proveedor === undefined || this.identificacion_proveedor === null) 
+          && (this.numero_contrato != undefined && this.numero_contrato != null) && (this.vigencia != undefined)){
             console.info('peticion por número de contrato y vigencia')
           }
         }
