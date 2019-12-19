05ed0fdee181e9028ef01e581c0ce6e34f2eb817
anlramirezs <anlramirezs11@gmail.com>
2019-12-19 07:08:29 -0500

    Se suma o resta el valor del select si depende de otro selector. Issue #18

--- src/app/components/plantilla-evaluacion/plantilla-evaluacion.component.ts
+++ src/app/components/plantilla-evaluacion/plantilla-evaluacion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import { LeerJsonLocalService } from '../../services/leer-json-local.service';
+import { identifierModuleUrl } from '@angular/compiler';
 
 @Component({
   selector: 'ngx-plantilla-evaluacion',
@@ -33,7 +34,16 @@ export class PlantillaEvaluacionComponent implements OnInit {
     this.json.Secciones[i].ValorSeccion = 0;
     for (let k = 0; k < this.json.Secciones[i].Seccion_hija_id.length; k++) {
       if (this.json.Secciones[i].Seccion_hija_id[k]['Item'][2].Valor.Valor !== undefined) {
-        this.json.Secciones[i].ValorSeccion += this.json.Secciones[i].Seccion_hija_id[k]['Item'][2].Valor.Valor;
+        if (this.json.Secciones[i].Seccion_hija_id[k]['Condicion'].length > 0) {
+          if (this.json.Secciones[i].Seccion_hija_id[k - 1]['Item'][2].Valor.Nombre ===
+          this.json.Secciones[i].Seccion_hija_id[k]['Condicion'][0]['Nombre']) {
+            this.json.Secciones[i].ValorSeccion += this.json.Secciones[i].Seccion_hija_id[k]['Item'][2].Valor.Valor;
+          } else {
+            this.json.Secciones[i].Seccion_hija_id[k]['Item'][2].Valor = '';
+          }
+        } else {
+          this.json.Secciones[i].ValorSeccion += this.json.Secciones[i].Seccion_hija_id[k]['Item'][2].Valor.Valor;
+        }
       }
     }
     for (let k = 0; k < this.json.Secciones.length; k++) {
