2ff7a0c92aa181c7f33b3d1ca4151ce70a00858c
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-12-22 18:03:56 -0500

    los guard funcionan pero toca realizar bien la validacion y dejarla mas robusta #15

--- src/app/@core/_guards/auth.guard.ts
+++ src/app/@core/_guards/auth.guard.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
@@ -9,35 +11,40 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
 
-      let valid: boolean =  false;
+      let valid: boolean =  true;
       const roles = route.data['roles'] as Array<string>;
+      console.info(`${route}`);
+      console.info(`${state}`);
       const id_token = window.localStorage.getItem('id_token').split('.');
+      // console.info(id_token);
       const payload = JSON.parse(atob(id_token[1]));
+      console.info(payload);
 
-      if (payload && payload.role) {
-        for ( let i = 0; i < payload.role.length; i++) {
-          for ( let j = 0; j < roles.length; j++) {
-              if ( payload.role[i] === roles[j]) {
-                  valid = true;
-                  break;
-              }
-          }
-        }
-      }
+      // if (payload && payload.role) {
+      //   for ( let i = 0; i < payload.role.length; i++) {
+      //     for ( let j = 0; j < roles.length; j++) {
+      //         if ( payload.role[i] === roles[j]) {
+      //             valid = true;
+      //             break;
+      //         }
+      //     }
+      //   }
+      // }
 
-      if (!valid) {
-        // not logged in so redirect to login page with the return url
-        // or not exist role return url
-        this.router.navigate(['/']);
-      }
+      // if (!valid) {
+      //   // not logged in so redirect to login page with the return url
+      //   // or not exist role return url
+      //   this.router.navigate(['/']);
+      // }
 
-      return valid;
+      return Observable.of(valid);
+      // return valid;
   }
 
-  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-      return this.canActivate(route, state);
-  }
+  // canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  //     return this.canActivate(route, state);
+  // }
 
 }
