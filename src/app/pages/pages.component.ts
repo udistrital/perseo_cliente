4c9146076f9ca4124c4126a56fe0029596ad36f0
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-12-26 11:22:37 -0500

    funciona la funcion para trer menu #27

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { MenuService } from '../@core/data/menu.service';
+
+
 
 import { MENU_ITEMS } from './pages-menu';
 
@@ -17,12 +20,19 @@ import { MENU_ITEMS } from './pages-menu';
   `,
 })
 export class PagesComponent {
+  menu = MENU_ITEMS;
 
-  constructor(private translate: TranslateService,
+  constructor(
+    private translate: TranslateService,
+    private menuService: MenuService,
     ) {
       this.translate.onLangChange.subscribe((event: LangChangeEvent) => { // Live reload
       });
+      // this.menuService.get(`ORDENADOR_DEL_GASTO/Evaluacion`).subscribe( menuResult => {
+      //   console.info(menuResult);
+      //   this.menu = menuResult;
+      // } );
+
     }
 
-  menu = MENU_ITEMS;
 }
