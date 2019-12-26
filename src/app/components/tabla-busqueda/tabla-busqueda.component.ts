6ea18f92741c6a894de04d0729786878a6918108
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-12-26 14:07:02 -0500

    se oculta boton de realizar evaluacion cuando no se esta logueado #15

--- src/app/components/tabla-busqueda/tabla-busqueda.component.ts
+++ src/app/components/tabla-busqueda/tabla-busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ImplicitAutenticationService } from '../../@core/utils/implicit_autentication.service';
 
 @Component({
   selector: 'ngx-tabla-busqueda',
@@ -9,16 +10,21 @@ export class TablaBusquedaComponent implements OnInit {
   @Input() dataContratos: any = [];
   @Output() dataToDo: any = {};
   @Output() dataView: any = {};
+  logueado: boolean = false;
 
 
-
-  constructor() {
+  constructor(
+    private implicitAutenticationService: ImplicitAutenticationService,
+  ) {
     this.dataToDo = new EventEmitter();
     this.dataView = new EventEmitter();
 
   }
 
   ngOnInit() {
+    if (this.implicitAutenticationService.live()) {
+      this.logueado = true;
+    }
   }
 
   realizarEvaluacion(data: any) {
