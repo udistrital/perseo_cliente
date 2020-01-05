df3ca0124db22b9756ea0502d2b8dae0a507d789
anlramirezs <anlramirezs11@gmail.com>
2020-01-05 14:38:46 -0500

    Se agrega el post a resultado evaluacion, se realiza la verificacion si el contrato ya tiene evaluacion. Issue #28

--- src/app/components/realizar-evaluacion/realizar-evaluacion.component.ts
+++ src/app/components/realizar-evaluacion/realizar-evaluacion.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, ViewChild, TemplateRef, EventEmitter } from '@angular/core';
 import { EvaluacioncrudService } from '../../@core/data/evaluacioncrud.service';
+import { NbWindowService } from '@nebular/theme';
+
 
 
 @Component({
@@ -10,12 +12,15 @@ import { EvaluacioncrudService } from '../../@core/data/evaluacioncrud.service';
 export class RealizarEvaluacionComponent implements OnInit {
   @Input() dataContrato: any = [];
   @Output() volverFiltro: EventEmitter<Boolean>;
+  @ViewChild('contentTemplate', { read: false }) contentTemplate: TemplateRef<any>;
+
   realizar: boolean;
   jsonEvaluacion: any;
   jsonResultadoEvaluacion: any;
 
   constructor(
     private evaluacionCrudService: EvaluacioncrudService,
+    private windowService: NbWindowService,
   ) {
     this.volverFiltro = new EventEmitter();
     this.jsonEvaluacion = {};
@@ -26,6 +31,20 @@ export class RealizarEvaluacionComponent implements OnInit {
 
   ngOnInit() {
     this.realizar = true;
+    console.info(this.dataContrato)
+    // Se verifica si se ha realizado una evaluación
+    this.evaluacionCrudService.get('evaluacion?query=ContratoSuscrito:' + this.dataContrato[0].ContratoSuscrito +
+      '&Vigencia:' + this.dataContrato[0].Vigencia).subscribe((res_evaluacion) => {
+        if (Object.keys(res_evaluacion[0]).length !== 0) {
+          this.openWindow('Ya hay una evaluación existente, usted procederá a modificarla.');
+        }
+      }, (error_service) => {
+        console.info(error_service);
+        this.openWindow(error_service.message);
+      });
+
+
+
   }
 
   regresarFiltro() {
@@ -42,19 +61,32 @@ export class RealizarEvaluacionComponent implements OnInit {
       'ProveedorId': this.dataContrato[0].IdProveedor,
       'Vigencia': this.dataContrato[0].Vigencia,
     };
-
+    // Se realiza el post de la evaluación.
     this.evaluacionCrudService.post('evaluacion', this.jsonEvaluacion)
-      .subscribe((res) => {
-        if (res !== null) {
+      .subscribe((res_evaluacion) => {
+        if (res_evaluacion !== null) {
+
           this.jsonResultadoEvaluacion = {
             'Activo': true,
-            'IdEvaluacion': res,
-            'ResultadoEvaluacion': data,
+            'IdEvaluacion': res_evaluacion,
+            'ResultadoEvaluacion': JSON.stringify(data),
           };
-          console.info(this.jsonResultadoEvaluacion);
+          // Se realiza el post del resultado de la evaluación.
+          this.evaluacionCrudService.post('resultado_evaluacion', this.jsonResultadoEvaluacion)
+            .subscribe((res_resultado_eva) => {
+              if (res_resultado_eva !== null) {
+                this.openWindow('Se ha registrado satisfactoriamente la evaluación del contrato ' + this.dataContrato[0].ContratoSuscrito +
+                  ' de ' + this.dataContrato[0].Vigencia);
+                this.regresarFiltro();
+              }
+            }, (error_service) => {
+              console.info(error_service);
+              this.openWindow(error_service.message);
+            });
         }
       }, (error_service) => {
         console.info(error_service);
+        this.openWindow(error_service.message);
       });
 
     // console.info(this.dataContrato);
@@ -68,6 +100,14 @@ export class RealizarEvaluacionComponent implements OnInit {
 
 
 
+  }
+
+
+  openWindow(mensaje) {
+    this.windowService.open(
+      this.contentTemplate,
+      { title: 'Alerta', context: { text: mensaje } },
+    );
   }
 
 }
