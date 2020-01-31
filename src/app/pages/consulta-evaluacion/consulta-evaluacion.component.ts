fc8d5035962b8489a9a2c8bd28f487f7c2f24c40
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-01-30 19:06:49 -0500

    implementacion de modulos para embeber

--- src/app/pages/consulta-evaluacion/consulta-evaluacion.component.ts
+++ src/app/pages/consulta-evaluacion/consulta-evaluacion.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, TemplateRef, ViewChild, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import { NbWindowService } from '@nebular/theme';
+import { EvaluacionmidService } from '../../@core/data/evaluacionmid.service';
+
 
 @Component({
   selector: 'ngx-consulta-evaluacion',
@@ -10,10 +13,28 @@ export class ConsultaEvaluacionComponent implements OnInit {
 
   identificacionProveedor: any;
   tipodocumento: any;
+  // data: any;
+  /*Se guarda los datos que envía el componente filtro*/
+  data: any;
+  /*Variables que guardan los datos que envía el componente tabla-busqueda*/
+  datosContratoAVer: any;
+  datosContratoAEvaluar: any;
+  /*Varible para saber si debe mostrar o no el componente ver*/
+  componenteVer: boolean;
+  /*Varible para saber si debe mostrar o no el componente ver*/
+  componenteRealizar: boolean;
+  @ViewChild('contentTemplate', { read: false }) contentTemplate: TemplateRef<any>;
 
   constructor(
     private activatedRoute: ActivatedRoute,
+    private windowService: NbWindowService,
+    private evaluacionMidService: EvaluacionmidService,
     ) {
+      this.data = [];
+    this.datosContratoAVer = [];
+    this.datosContratoAEvaluar = [];
+      this.componenteRealizar = false;
+      this.componenteVer = false;
       this.activatedRoute.params.subscribe( params => {
         this.identificacionProveedor = params['IdentificacionProveedor'];
         this.tipodocumento = params['TipoDocumento'];
@@ -22,6 +43,39 @@ export class ConsultaEvaluacionComponent implements OnInit {
      }
 
   ngOnInit() {
+    this.evaluacionMidService.get(`filtroProveedor?ProvID=${this.identificacionProveedor}&SupID=0` )
+        .subscribe((res) => {
+          if (res !== null) {
+            this.data = res;
+          }
+        }, (error_service) => {
+          this.openWindow(error_service['body'][1]['Error']);
+          this.data = [];
+        });
+  }
+
+  openWindow(mensaje) {
+    this.windowService.open(
+      this.contentTemplate,
+      { title: 'Alerta', context: { text: mensaje } },
+    );
+  }
+
+  habilitarFiltro(data: any) {
+    if (data === true ) {
+      this.componenteVer = false;
+      this.componenteRealizar = false;
+    }
+  }
+
+  verEvaluacion(data: any) {
+    this.datosContratoAVer[0] = data;
+    this.componenteVer = true;
+  }
+
+  relizarEvaluacion(data: any) {
+    this.datosContratoAEvaluar[0] = data;
+    this.componenteRealizar = true;
   }
 
 }
